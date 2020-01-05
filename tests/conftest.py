7399392257b6bde3ccd0a5586a42d82802ffc33a
rmk2 <ryko@rmk2.org>
2020-01-05 23:30:21 +0100

    Extend tests for database.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
+import os.path
+from typing import Tuple
+
 import pytest
+from sqlalchemy import MetaData
+from sqlalchemy.engine import Engine
 
 from gravity.config import BaseConfig
+from gravity.database import _metadata, get_engine
 
 
 @pytest.fixture(scope='module')
@@ -10,3 +16,16 @@ def prepared_config() -> BaseConfig:
     config(['test'])
 
     return config
+
+
+@pytest.fixture(scope='function')
+def test_database(prepared_config, tmpdir) -> Tuple[BaseConfig, Engine, MetaData]:
+    """Prepare a test database"""
+    _config = prepared_config
+
+    _config.set_override(name='driver', override='sqlite', group='backend')
+    _config.set_override(name='database', override=os.path.join(tmpdir, 'test_database.sqlite'), group='sqlite')
+
+    _engine = get_engine(_config)
+
+    yield _config, _engine, _metadata
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 from sqlalchemy.engine import Engine, url
 
 import gravity.database
+from gravity.model import project
 
 
 def test_get_engine_sqlite(prepared_config) -> None:
+    """Check that getting a SQLAlchemy engine for SQLite meets expectations"""
     config = prepared_config
     config.set_override(name='driver', override='sqlite', group='backend')
     config.set_override(name='database', override='test_database.sqlite', group='sqlite')
@@ -20,6 +24,7 @@ def test_get_engine_sqlite(prepared_config) -> None:
 
 
 def test_get_engine_postgresql(prepared_config) -> None:
+    """Check that getting a SQLAlchemy engine for PostgreSQL meets expectations"""
     config = prepared_config
     config.set_override(name='driver', override='postgresql', group='backend')
     config.set_override(name='hostname', override='localhost', group='postgresql')
@@ -46,6 +51,7 @@ def test_get_engine_postgresql(prepared_config) -> None:
 
 
 def test_get_engine_none(prepared_config) -> None:
+    """Check that getting an empty engine for non-database backends meets expectations"""
     config = prepared_config
     config.set_override(name='driver', override='csv', group='backend')
 
@@ -54,17 +60,87 @@ def test_get_engine_none(prepared_config) -> None:
     assert engine is None
 
 
-def test_drop() -> None:
-    pass
+def test_initialise(test_database) -> None:
+    """Check that initialising all database tables meets expectations"""
+    config, engine, metadata = test_database
+
+    gravity.database.initialise(config)
+
+    for table in metadata.tables:
+        _table = metadata.tables[table]
+
+        assert engine.has_table(_table.name)
 
 
-def test_initialise() -> None:
-    pass
+def test_drop(test_database) -> None:
+    """Check that dropping all database tables meets expectations"""
+    config, engine, metadata = test_database
+
+    gravity.database.initialise(config)
+    gravity.database.drop(config)
+
+    for table in metadata.tables:
+        _table = metadata.tables[table]
+
+        assert not engine.has_table(_table.name)
 
 
-def test_prune() -> None:
-    pass
+def test_prune(test_database) -> None:
+    """Check that deleting entries that have been marked for deletion meets expectations"""
+    config, engine, metadata = test_database
+
+    gravity.database.initialise(config)
+
+    # Insert synthetic rows
+    _now = datetime.now()
+    _keys = [x.name for x in gravity.model.project.columns]
+    _select = gravity.model.project.select()
+
+    rows = [
+        ['1', 'foo', 'foo description', 'FOO', _now, _now, _now],
+        ['2', 'bar', 'bar description', 'BAR', _now, _now, None],
+    ]
+    rows = [dict(zip(_keys, x)) for x in rows]
+
+    engine.execute(gravity.model.project.insert(), rows)
+
+    # Check that rows have been inserted
+    assert [dict(x) for x in engine.execute(_select).fetchall()] == rows
+
+    # Prune rows marked for deletion
+    gravity.database.prune(config)
+
+    # Check that rows have been pruned
+    assert [dict(x) for x in engine.execute(_select).fetchall()] == [x for x in rows if x['deleted'] is None]
+    assert [dict(x) for x in engine.execute(_select).fetchall()] != [x for x in rows if x['deleted'] is not None]
 
 
-def test_truncate() -> None:
-    pass
+def test_truncate(test_database) -> None:
+    """Check that deleting all entries in a table without dropping the table meets expectations"""
+    config, engine, metadata = test_database
+
+    gravity.database.initialise(config)
+
+    # Insert synthetic rows
+    _now = datetime.now()
+    _keys = [x.name for x in gravity.model.project.columns]
+    _select = gravity.model.project.select()
+
+    rows = [
+        ['1', 'foo', 'foo description', 'FOO', _now, _now, _now],
+        ['2', 'bar', 'bar description', 'BAR', _now, _now, None],
+    ]
+    rows = [dict(zip(_keys, x)) for x in rows]
+
+    engine.execute(gravity.model.project.insert(), rows)
+
+    # Check that rows have been inserted
+    assert [dict(x) for x in engine.execute(_select).fetchall()] == rows
+
+    # Truncate all rows
+    gravity.database.truncate(config)
+
+    # Check that all rows have been deleted
+    assert len(engine.execute(_select).fetchall()) == 0
+    assert engine.execute(_select).fetchall() == []
+
